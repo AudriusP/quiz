c71fb267d5ca47e677da47e23f4a7170bc9b60ac
Audrius Petreikis <audriusp@wix.com>
2017-08-14 15:16:57 +0300

    Refactored functions - splitted to smaller functions

--- quiz.js
+++ quiz.js
@@ -29,47 +29,47 @@ var currentQuestion = 0;
 
 function next() {
     if (isAnyChecked()){
-        allQuestions[currentQuestion].userAnswer = checked();
-        if (radioButtons[allQuestions[currentQuestion].correctAnswer].checked && !(allQuestions[currentQuestion].alreadyAnswredCorrectly)){
-            correctAnswers++;
-            allQuestions[currentQuestion].alreadyAnswredCorrectly = true;
+        recordUserAnswer();
+        if (isAnswerCorrect() && notYetAnsweredCorrectly()){
+            userAnsweredCorrectly();
         }
-        if(currentQuestion != allQuestions.length - 1) {
-            currentQuestion++;
-            setQuestion(currentQuestion);
+        if(notLastQuestion()) {
+            nextQuestion();
         }
         else{
-            finishContainer.innerHTML = ("You answered " + correctAnswers + " questions correctly!");
-            quizContainer.remove();
+            quizFinish();
         }
     }
     else {
-        finishContainer.innerHTML = "Choose!";
+        answerNotChosen();
     }
 }
 
 function back() {
-        if(currentQuestion != 0) {
-            currentQuestion--;
-            setQuestion(currentQuestion);
+        if(notFirstQuestion()) {
+            previousQuestion();
         }
         else{
-            finishContainer.innerHTML = "This is first question!";
+            noMorePreviousQuestions();
         }
 }
 
 function setQuestion (id){
     questionContainer.innerHTML = allQuestions[id].question;
-    for (var i = 0; i < choicesContainers.length; i++ ){
+    setAnswers(id);
+
+}
+
+function setAnswers(id) {
+    for (var i = 0; i < choicesContainers.length; i++) {
         choicesContainers[i].innerHTML = allQuestions[id].choices[i];
-        if (allQuestions[id].userAnswer || allQuestions[id].userAnswer == 0) {
+        if (allQuestions[id].userAnswer || allQuestions[id].userAnswer === 0) {
             radioButtons[allQuestions[id].userAnswer].checked = true;
         }
         else {
             radioButtons[i].checked = false;
         }
     }
-
 }
 
 function isAnyChecked (){
@@ -82,7 +82,7 @@ function isAnyChecked (){
     return false;
 }
 
-function checked (){
+function whichIsChecked (){
     for (var i = 0; i < radioButtons.length; i++) {
         if (radioButtons[i].checked) {
             return i;
@@ -95,25 +95,24 @@ function showData() {
 }
 
 function login(){
-    if(userNameContainer.value && passwordContainer.value) {
-        if (!(localStorage.getItem("userName")) && !(localStorage.getItem("password"))) {
-            localStorage.setItem("userName", userNameContainer.value);
-            localStorage.setItem("password", passwordContainer.value);
-            finishContainer.innerHTML = ("Hello first time " + localStorage.getItem("userName"));
+    if(loginNotEmpty()) {
+        if (newUser()) {
+            setLocalStorage();
+            loggedInFirstTimeGreeting();
             showData();
-            document.cookie = "username=" + userNameContainer.value;
+            setCookie();
         }
-        else if (localStorage.getItem("userName") == userNameContainer.value && localStorage.getItem("password") == passwordContainer.value) {
-            finishContainer.innerHTML = ("Hello again " + localStorage.getItem("userName"));
+        else if (registeredUser()) {
+            loggedInAgainGreeting();
             showData();
-            document.cookie = "username=" + userNameContainer.value;
+            setCookie();
         }
         else {
-            finishContainer.innerHTML = ("Wrong data");
+            userNotFound();
         }
     }
     else {
-        finishContainer.innerHTML = ("No data");
+        loginFieldsEmpty();
     }
 }
 
@@ -122,10 +121,10 @@ function getCookie(cookieName) {
     var splitCookie = document.cookie.split(';');
     for (var i = 0; i < splitCookie.length; i++){
         var currentCookie = splitCookie[i];
-        if (currentCookie.charAt(0) == " "){
+        if (currentCookie.charAt(0) === " "){
             currentCookie = currentCookie.substring(1);
         }
-        if (currentCookie.indexOf(name) == 0){
+        if (currentCookie.indexOf(name) === 0){
             return currentCookie.substring(name.length);
         }
     }
@@ -133,10 +132,95 @@ function getCookie(cookieName) {
 }
 
 function checkCookie() {
-    if (getCookie("username") != ""){
+    if (getCookie("username") !== ""){
         finishContainer.innerHTML = ("Welcome " + getCookie("username"));
     }
     else {
         finishContainer.innerHTML = ("Welcome, new user!");
     }
+}
+
+function isAnswerCorrect() {
+    return radioButtons[allQuestions[currentQuestion].correctAnswer].checked;
+}
+
+function notYetAnsweredCorrectly() {
+    return !(allQuestions[currentQuestion].alreadyAnswredCorrectly);
+}
+
+function recordUserAnswer() {
+    allQuestions[currentQuestion].userAnswer = whichIsChecked();
+}
+
+function userAnsweredCorrectly() {
+    correctAnswers++;
+    allQuestions[currentQuestion].alreadyAnswredCorrectly = true;
+}
+
+function nextQuestion() {
+    currentQuestion++;
+    setQuestion(currentQuestion);
+}
+
+function quizFinish() {
+    finishContainer.innerHTML = ("You answered " + correctAnswers + " questions correctly!");
+    quizContainer.remove();
+}
+
+function notLastQuestion() {
+    return currentQuestion !== (allQuestions.length - 1);
+}
+
+function answerNotChosen() {
+    finishContainer.innerHTML = "Choose answer!";
+}
+
+function notFirstQuestion() {
+    return currentQuestion !== 0;
+}
+
+function previousQuestion() {
+    currentQuestion--;
+    setQuestion(currentQuestion);
+}
+
+function noMorePreviousQuestions() {
+    finishContainer.innerHTML = "This is first question!";
+}
+
+function loginNotEmpty() {
+    return userNameContainer.value && passwordContainer.value;
+}
+
+function newUser() {
+    return !(localStorage.getItem("userName")) && !(localStorage.getItem("password"));
+}
+
+function setLocalStorage() {
+    localStorage.setItem("userName", userNameContainer.value);
+    localStorage.setItem("password", passwordContainer.value);
+}
+
+function setCookie() {
+    document.cookie = "username=" + userNameContainer.value;
+}
+
+function loggedInFirstTimeGreeting() {
+    finishContainer.innerHTML = ("Hello first time " + localStorage.getItem("userName"));
+}
+
+function loggedInAgainGreeting() {
+    finishContainer.innerHTML = ("Hello again " + localStorage.getItem("userName"));
+}
+
+function userNotFound() {
+    finishContainer.innerHTML = ("User not found / Wrong user name or password");
+}
+
+function loginFieldsEmpty() {
+    finishContainer.innerHTML = ("No data");
+}
+
+function registeredUser() {
+    return localStorage.getItem("userName") === userNameContainer.value && localStorage.getItem("password") === passwordContainer.value;
 }
